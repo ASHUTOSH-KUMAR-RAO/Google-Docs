44bd55f912cd101fa81998caf7c4fa544b9c3601
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-06-19 21:23:37 +0530

    Adding Home Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Navbar } from "./navbar";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed left-0 top-0 right-0 z-10 h-16 bg-white">
+        <Navbar/>
+      </div>
+      <div className="mt-16">
+
       <Button variant="destructive" >
         Click{" "}
         <Link href="/Documents/1244">
@@ -11,6 +17,7 @@ const Home = () => {
         </Link>{" "}
         And Go To the Documents Page
       </Button>
+    </div>
     </div>
   );
 };
